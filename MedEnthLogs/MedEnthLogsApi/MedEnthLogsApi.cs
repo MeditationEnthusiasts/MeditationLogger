d373bc7edabb5988ed9f836604641d4ee8ad8699
Seth Hendrick <seth@shendrick.net>
2015-08-13 17:54:43 -0400

    Initial commit of Visual Studio project for both the API and the Windows 10 universal app.

---
+++ MedEnthLogs/MedEnthLogsApi/MedEnthLogsApi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MedEnthLogsApi
+{
+    public class MedEnthLogsApi
+    {
+        public MedEnthLogsApi()
+        {
+
+        }
+    }
+}
