12f658d1519f0e1c24c4979265a56ce75e110687
Seth Hendrick <seth@shendrick.net>
2015-08-13 23:46:09 -0400

    Added Unit Test Project.

---
+++ MedEnthLogs/Test/LogTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using MedEnthLogsApi;
+using NUnit.Framework;
+
+namespace TestCommon
+{
+    /// <summary>
+    /// Tests the Log Class.
+    /// </summary>
+    [TestFixture]
+    public class LogTest
+    {
+        /// <summary>
+        /// Unit under test.
+        /// </summary>
+        Log uut;
+
+        [SetUp]
+        public void TestSetup()
+        {
+            uut = new Log();
+        }
+
+        /// <summary>
+        /// Ensures that the duration property is the end time minus the start time.
+        /// </summary>
+        [Test]
+        public void TestDuration()
+        {
+            DateTime start = DateTime.MinValue;
+            DateTime end = DateTime.MaxValue;
+
+            uut.StartTime = start;
+            uut.EndTime = end;
+
+            TimeSpan expected = end - start;
+
+            Assert.AreEqual( expected, uut.Duration );
+        }
+    }
+}
