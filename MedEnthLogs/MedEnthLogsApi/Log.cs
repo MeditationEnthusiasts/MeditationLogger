8ef3bd37a9a47911a735635d74c62bb928ec1fa6
Seth Hendrick <seth@shendrick.net>
2015-08-13 22:47:04 -0400

    Added stubs for Log and LogBook.

---
+++ MedEnthLogs/MedEnthLogsApi/Log.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MedEnthLogsApi
+{
+    /// <summary>
+    /// This class represents a specific instance
+    /// of an event logged.
+    /// </summary>
+    public class Log
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Log()
+        {
+            this.StartTime = DateTime.MinValue;
+            this.EndTime = DateTime.MinValue;
+            this.Comments = string.Empty;
+            this.Location = string.Empty;
+        }
+
+        /// <summary>
+        /// When the log starts
+        /// </summary>
+        public DateTime StartTime { get; set; }
+
+        /// <summary>
+        /// When the log ends.
+        /// </summary>
+        public DateTime EndTime { get; set; }
+
+        /// <summary>
+        /// How long the session lasted.
+        /// </summary>
+        public TimeSpan Duration
+        {
+            get
+            {
+                return EndTime - StartTime;
+            }
+        }
+
+        /// <summary>
+        /// The comments the user wrote about the session.
+        /// </summary>
+        public string Comments { get; set; }
+
+        /// <summary>
+        /// Where the user had the session.
+        /// This can either be GPS coordinates or a location the user
+        /// specifies.
+        /// </summary>
+        public string Location { get; set; }
+    }
+}
---
+++ MedEnthLogs/MedEnthLogsApi/LogBook.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace MedEnthLogsApi
+{
+    /// <summary>
+    /// This class represents a book of logs.
+    /// </summary>
+    public class LogBook
+    {
+        // -------- Fields --------
+
+        /// <summary>
+        /// List of logs.
+        /// </summary>
+        List<Log> logs;
+
+        // -------- Constructor --------
+
+        /// <summary>
+        /// Constructor.
+        /// Creates an empty log list.
+        /// </summary>
+        public LogBook()
+        {
+            this.logs = new List<Log>();
+            this.Logs = this.logs.AsReadOnly();
+        }
+
+        // --------- Properties --------
+
+        /// <summary>
+        /// Readonly List of logs.
+        /// </summary>
+        public IList<Log> Logs { get; private set; }
+
+        // -------- Functions --------
+        
+        /// <summary>
+        /// Loads the logs from the specified file location.
+        /// </summary>
+        /// <param name="logLocation">
+        /// The location of the logs.
+        /// The file name extension of this determines how the logs are loaded.
+        /// </param>
+        public void LoadLogs( string fileLocation )
+        {
+            throw new NotImplementedException( "LoadLogs not implemented yet." );
+        }
+    }
+}
