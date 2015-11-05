408b91cff7d420a1cde49aa99fd48a0164eedc42
Seth Hendrick <seth@shendrick.net>
2015-11-04 21:16:45 -0500

    Added icon for win32 shortcuts and executable

--- MedEnthLogs/MedEnthDesktop/Properties/AssemblyInfo.cs
+++ MedEnthLogs/MedEnthDesktop/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle( "MedEnthDesktop" )]
-[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyTitle( "Meditation Logger" )]
+[assembly: AssemblyDescription( "A way for meditators to log their sessions." )]
 [assembly: AssemblyConfiguration( "" )]
-[assembly: AssemblyCompany( "" )]
-[assembly: AssemblyProduct( "MedEnthDesktop" )]
+[assembly: AssemblyCompany( "Meditation Enthusiasts" )]
+[assembly: AssemblyProduct( "Meditation Logger" )]
 [assembly: AssemblyCopyright( "Copyright ©  2015" )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "1.0.0.0" )]
-[assembly: AssemblyFileVersion( "1.0.0.0" )]
+[assembly: AssemblyVersion( MedEnthLogsApi.Api.Version )]
+[assembly: AssemblyFileVersion( MedEnthLogsApi.Api.Version )]
