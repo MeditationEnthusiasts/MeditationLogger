1e926bd4200f6e9c38556ddeff105d246229f2e0
Seth Hendrick <seth@shendrick.net>
2015-11-30 21:45:53 -0500

    Windows version is back online.
    Needed to add the Win32Program partial class.

---
+++ MedEnthLogs/MedEnthDesktop/Win32Program.cs
@@ -0,0 +1,49 @@
+﻿// 
+// Meditation Logger.
+// Copyright (C) 2015  Seth Hendrick.
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+using MedEnthDesktop;
+using MedEnthLogsApi;
+
+namespace MedEnthLogsDesktop
+{
+    static partial class Program
+    {
+        /// <summary>
+        /// Gets a linux API.
+        /// </summary>
+        /// <returns>The Linux API.</returns>
+        static Api GetApi()
+        {
+            return new Api(
+                new Win32LocationDetector(),
+                new Win32Timer(),
+                new NAudioMusicManager(),
+                new SQLite.Net.Platform.Win32.SQLitePlatformWin32()
+            );
+        }
+
+        /// <summary>
+        /// Returns the Music Manager to use for Linux.
+        /// </summary>
+        /// <returns>The music manager for linux.</returns>
+        static IMusicManager GetMusicManager()
+        {
+            return new NAudioMusicManager();
+        }
+    }
+}
