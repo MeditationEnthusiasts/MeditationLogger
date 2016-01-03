1e22987bf25326717bf880b523e0ba194546ff5a
Seth Hendrick <seth@shendrick.net>
2016-01-02 22:33:57 -0500

    Got the Windows build compiling again.
    All Unit Tests still pass.

---
+++ MedEnthLogs/MedEnthLogsCli/Win32Program.cs
@@ -0,0 +1,52 @@
+﻿// 
+// Meditation Logger.
+// Copyright (C) 2015  Seth Hendrick.
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+using System;
+using MedEnthDesktop;
+using MedEnthLogsApi;
+using MedEnthLogsDesktop;
+
+namespace MedEnthLogsCli
+{
+    static partial class Program
+    {
+        /// <summary>
+        /// Gets a linux API.
+        /// </summary>
+        /// <returns>The Linux API.</returns>
+        static Api GetApi()
+        {
+            return new MedEnthLogsApi.Api(
+                new Win32LocationDetector(),
+                new Win32Timer(),
+                new NAudioMusicManager(),
+                new SQLite.Net.Platform.Win32.SQLitePlatformWin32()
+            );
+        }
+
+        /// <summary>
+        /// Returns the Music Manager to use for Linux.
+        /// </summary>
+        /// <returns>The music manager for linux.</returns>
+        static IMusicManager GetMusicManager()
+        {
+            return new NAudioMusicManager();
+        }
+    }
+}
+
---
+++ MedEnthLogs/Test/Win32ApiTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MedEnthLogsApi;
+using MedEnthLogsDesktop;
+
+namespace TestCommon
+{
+    public partial class LogsApiTest
+    {
+        public Api GetApi()
+        {
+            return new Api(
+                new Win32LocationDetector(),
+                this.mockTimer,
+                this.mockAudio,
+                new SQLite.Net.Platform.Win32.SQLitePlatformWin32()
+            );
+        }
+
+        private const string projectDir = @"..\..\";
+    }
+}
+
