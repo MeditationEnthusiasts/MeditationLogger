b8ba728d79638d4eeb593e1625c4a21e1fe8c43a
Seth Hendrick <seth@shendrick.net>
2016-01-30 19:23:26 -0500

    Issue: 13 Updated the save view so it expands when the window is resized.

--- MedEnthLogs/MedEnthDesktop/SaveView.Designer.cs
+++ MedEnthLogs/MedEnthDesktop/SaveView.Designer.cs
@@ -53,13 +53,15 @@ namespace MedEnthDesktop
             this.TechniqueUsedTextbox = new System.Windows.Forms.TextBox();
             this.CommentsLabel = new System.Windows.Forms.Label();
             this.CommentsTextBox = new System.Windows.Forms.RichTextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // MinutesLabel
             // 
             this.MinutesLabel.AutoSize = true;
             this.MinutesLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.MinutesLabel.Location = new System.Drawing.Point(17, 12);
+            this.MinutesLabel.Location = new System.Drawing.Point(3, 0);
             this.MinutesLabel.Name = "MinutesLabel";
             this.MinutesLabel.Size = new System.Drawing.Size(105, 16);
             this.MinutesLabel.TabIndex = 0;
@@ -69,7 +71,7 @@ namespace MedEnthDesktop
             // 
             this.MinutesValueLabel.AutoSize = true;
             this.MinutesValueLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.MinutesValueLabel.Location = new System.Drawing.Point(128, 12);
+            this.MinutesValueLabel.Location = new System.Drawing.Point(153, 0);
             this.MinutesValueLabel.Name = "MinutesValueLabel";
             this.MinutesValueLabel.Size = new System.Drawing.Size(23, 15);
             this.MinutesValueLabel.TabIndex = 1;
@@ -78,7 +80,8 @@ namespace MedEnthDesktop
             // UseLocationCheckbox
             // 
             this.UseLocationCheckbox.AutoSize = true;
-            this.UseLocationCheckbox.Location = new System.Drawing.Point(18, 211);
+            this.tableLayoutPanel1.SetColumnSpan(this.UseLocationCheckbox, 2);
+            this.UseLocationCheckbox.Location = new System.Drawing.Point(3, 213);
             this.UseLocationCheckbox.Name = "UseLocationCheckbox";
             this.UseLocationCheckbox.Size = new System.Drawing.Size(95, 17);
             this.UseLocationCheckbox.TabIndex = 2;
@@ -89,7 +92,7 @@ namespace MedEnthDesktop
             // 
             this.TechniqueUsedLabel.AutoSize = true;
             this.TechniqueUsedLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TechniqueUsedLabel.Location = new System.Drawing.Point(17, 39);
+            this.TechniqueUsedLabel.Location = new System.Drawing.Point(3, 30);
             this.TechniqueUsedLabel.Name = "TechniqueUsedLabel";
             this.TechniqueUsedLabel.Size = new System.Drawing.Size(126, 16);
             this.TechniqueUsedLabel.TabIndex = 3;
@@ -97,7 +100,7 @@ namespace MedEnthDesktop
             // 
             // TechniqueUsedTextbox
             // 
-            this.TechniqueUsedTextbox.Location = new System.Drawing.Point(149, 38);
+            this.TechniqueUsedTextbox.Location = new System.Drawing.Point(153, 33);
             this.TechniqueUsedTextbox.Name = "TechniqueUsedTextbox";
             this.TechniqueUsedTextbox.Size = new System.Drawing.Size(150, 20);
             this.TechniqueUsedTextbox.TabIndex = 4;
@@ -106,7 +109,7 @@ namespace MedEnthDesktop
             // 
             this.CommentsLabel.AutoSize = true;
             this.CommentsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CommentsLabel.Location = new System.Drawing.Point(17, 64);
+            this.CommentsLabel.Location = new System.Drawing.Point(3, 60);
             this.CommentsLabel.Name = "CommentsLabel";
             this.CommentsLabel.Size = new System.Drawing.Size(84, 16);
             this.CommentsLabel.TabIndex = 5;
@@ -114,26 +117,47 @@ namespace MedEnthDesktop
             // 
             // CommentsTextBox
             // 
-            this.CommentsTextBox.Location = new System.Drawing.Point(18, 88);
+            this.tableLayoutPanel1.SetColumnSpan(this.CommentsTextBox, 2);
+            this.CommentsTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.CommentsTextBox.Location = new System.Drawing.Point(3, 93);
             this.CommentsTextBox.Name = "CommentsTextBox";
-            this.CommentsTextBox.Size = new System.Drawing.Size(298, 106);
+            this.CommentsTextBox.Size = new System.Drawing.Size(344, 114);
             this.CommentsTextBox.TabIndex = 6;
             this.CommentsTextBox.Text = "";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.MinutesLabel, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.CommentsTextBox, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.MinutesValueLabel, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.UseLocationCheckbox, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.CommentsLabel, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.TechniqueUsedLabel, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.TechniqueUsedTextbox, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 75F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(350, 250);
+            this.tableLayoutPanel1.TabIndex = 7;
             // 
             // SaveView
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.Controls.Add(this.CommentsTextBox);
-            this.Controls.Add(this.CommentsLabel);
-            this.Controls.Add(this.TechniqueUsedTextbox);
-            this.Controls.Add(this.TechniqueUsedLabel);
-            this.Controls.Add(this.UseLocationCheckbox);
-            this.Controls.Add(this.MinutesValueLabel);
-            this.Controls.Add(this.MinutesLabel);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "SaveView";
             this.Size = new System.Drawing.Size(350, 250);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -146,5 +170,6 @@ namespace MedEnthDesktop
         public System.Windows.Forms.RichTextBox CommentsTextBox;
         public System.Windows.Forms.Label MinutesValueLabel;
         public System.Windows.Forms.CheckBox UseLocationCheckbox;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
