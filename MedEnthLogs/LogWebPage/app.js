4d469d8a63abf99e7aefea28851fd11ab50ffd55
Seth Hendrick <seth@shendrick.net>
2017-01-07 18:07:30 -0500

    Issue: #42 Updated copyright for the desktop application.

--- MedEnthLogs/LogWebPage/app.js
+++ MedEnthLogs/LogWebPage/app.js
@@ -1,6 +1,6 @@
 // 
 // Meditation Logger.
-// Copyright (C) 2015-2016  Seth Hendrick.
+// Copyright (C) 2015-2017  Seth Hendrick.
 // 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
